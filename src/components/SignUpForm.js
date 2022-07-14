ee91b2c89311f5d7076ff14bf5e113dd2749aced
rasool700 <rasoolkarami700@gmail.com>
2022-07-14 18:24:15 +0430

    yup added and form ux details added and radio button input added to project

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,43 +1,49 @@
 import React from "react";
 import { useFormik } from "formik";
-
+import * as Yup from "yup";
 //step1
 const initialValues = {
   name: "",
   email: "",
   password: "",
+  phoneNumber: "",
+  passwordConfirmation: "",
+  Gender: "",
 };
 //step2
 const onSubmit = (values) => console.log(values);
+//step3 with yup
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string()
+    .email("Email format is not correct")
+    .required("Email is required!"),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+  phoneNumber: Yup.string()
+    .required("Phone Number is required!")
+    .matches(
+      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+      "Phone number is not valid"
+    )
+    .min(11, "to short")
+    .max(11, "to long"),
 
-//step3 : validation
-const validate = (values) => {
-  let errors = {};
-  if (!values.name) {
-    errors.name = "name is required!";
-  }
-  if (!values.email) {
-    errors.email = "email is required!";
-  }
-  if (!values.password) {
-    errors.password = "password is required!";
-  }
-  return errors;
-};
+  passwordConfirmation: Yup.string()
+    .required("Renter your Password!")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  Gender: Yup.string().required("grnder is required"),
+});
 
 const SignUpForm = () => {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
+    validateOnMount: true,
   });
-  //   console.log(formik.values);
-
-  //   const submitHandler = (e) => {
-  //     e.preventDefault();
-  //     console.log("hello");
-  console.log("visied", formik.touched);
-  //   };
 
   return (
     <div>
@@ -58,7 +64,7 @@ const SignUpForm = () => {
         <div className="form-control">
           <label>Email</label>
           <input
-            type="text"
+            type="email"
             onChange={formik.handleChange}
             value={formik.values.email}
             onBlur={formik.handleBlur}
@@ -66,6 +72,19 @@ const SignUpForm = () => {
           ></input>
           {formik.errors.email && formik.touched.email && (
             <p className="error">{formik.errors.email}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Phone Number</label>
+          <input
+            type="phoneNumber"
+            onChange={formik.handleChange}
+            value={formik.values.phoneNumber}
+            onBlur={formik.handleBlur}
+            name="phoneNumber"
+          ></input>
+          {formik.errors.phoneNumber && formik.touched.phoneNumber && (
+            <p className="error">{formik.errors.phoneNumber}</p>
           )}
         </div>
         <div className="form-control">
@@ -81,7 +100,49 @@ const SignUpForm = () => {
             <p className="error">{formik.errors.password}</p>
           )}
         </div>
-        <button type="submit">Submit</button>
+        <div className="form-control">
+          <label>Password confirmation</label>
+          <input
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.passwordConfirmation}
+            onBlur={formik.handleBlur}
+            name="passwordConfirmation"
+          ></input>
+          {formik.errors.passwordConfirmation &&
+            formik.touched.passwordConfirmation && (
+              <p className="error">{formik.errors.passwordConfirmation}</p>
+            )}
+        </div>
+        <div className="form-container">
+          <input
+            type="radio"
+            id="0"
+            name="Gender"
+            value="0"
+            onChange={formik.handleChange}
+            checked={formik.values.Gender === "0"}
+          ></input>
+          <label htmlFor="0">Male</label>
+
+          <input
+            type="radio"
+            id="1"
+            name="Gender"
+            value="1"
+            onChange={formik.handleChange}
+            checked={formik.values.Gender === "1"}
+          ></input>
+          <label htmlFor="1">Female</label>
+        </div>
+
+        <button
+          type="submit"
+          disabled={!formik.isValid}
+          className={!formik.isValid ? "disabeled-btn" : ""}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
